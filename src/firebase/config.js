bfedf60eb0a410e88aea1744655d7009c85f213d
Tiago Ferrari <tiagoggferrari@gmail.com>
2024-08-05 15:35:05 -0300

    Config Firebase Auth

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLpXCTvuwqc0C8Ifxmu-x_HyHQCL8qGLQ",
+  authDomain: "satsyou-1f741.firebaseapp.com",
+  projectId: "satsyou-1f741",
+  storageBucket: "satsyou-1f741.appspot.com",
+  messagingSenderId: "251229709691",
+  appId: "1:251229709691:web:b93428ac98a7b52ba589de"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth_mod = getAuth(app);
+
+export { auth_mod };
+
+export default app;
